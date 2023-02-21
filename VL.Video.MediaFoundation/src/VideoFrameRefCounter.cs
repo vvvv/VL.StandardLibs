9338bd716c84a9561500805794693afb6c92dfb2
Elias Holzer <elias@vvvv.org>
2023-02-21 20:38:04 +0100

    Adds VL.Video.MediaFoundation

---
+++ VL.Video.MediaFoundation/src/VideoFrameRefCounter.cs
@@ -0,0 +1,23 @@
+﻿using VL.Lib.Basics.Resources;
+
+namespace VL.Video.MediaFoundation
+{
+    sealed class VideoFrameRefCounter : IRefCounter<VideoFrame>
+    {
+        public static readonly VideoFrameRefCounter Default = new VideoFrameRefCounter();
+
+        public void Init(VideoFrame resource)
+        {
+        }
+
+        public void AddRef(VideoFrame resource)
+        {
+            resource?.AddRef();
+        }
+
+        public void Release(VideoFrame resource)
+        {
+            resource?.Release();
+        }
+    }
+}
