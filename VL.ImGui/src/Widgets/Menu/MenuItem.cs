b1c833a46c317416bb706894636a02eba4528605
gregsn <gregor@vvvv.org>
2023-03-03 17:03:27 +0100

    MenuItem by default is not getting a checkbox when clicked. User needs to use "Selectable" to turn on the feature. From then on the "IsSelected" Channel establishes the two-way-binding to whether or not to display that checbox. Clicking will flip the IsSelected property again. Like it has been before.

--- VL.ImGui/src/Widgets/Menu/MenuItem.cs
+++ VL.ImGui/src/Widgets/Menu/MenuItem.cs
@@ -1,4 +1,6 @@
-﻿namespace VL.ImGui.Widgets
+﻿using VL.Lib.Reactive;
+
+namespace VL.ImGui.Widgets
 {
     /// <summary>
     /// Create a MenuItem. Keyboardshortcuts are displayed as a convenience but not processed by Dear ImGui at the moment.
@@ -6,18 +8,32 @@
     [GenerateNode(Category = "ImGui.Widgets")]
     internal partial class MenuItem : ChannelWidget<bool>
     {
-
         public string? Label { get; set; }
 
         public string? Shortcut { get; set; }
 
         public bool Enabled { get; set; } = true;
 
+        public bool Selectable { get; set; }
+
+        public Channel<bool>? IsSelected { private get; set; }
+        ChannelFlange<bool> IsSelectedFlange = new ChannelFlange<bool>(true);
+
         internal override void UpdateCore(Context context)
         {
             var value = Update();
-            if (ImGuiNET.ImGui.MenuItem(Context.GetLabel(this, Label), Shortcut, ref value, Enabled))
+
+            if (Selectable)
+            {
+                var isSelected = IsSelectedFlange.Update(IsSelected);
+                if (ImGuiNET.ImGui.MenuItem(Context.GetLabel(this, Label), Shortcut, ref isSelected, enabled: Enabled))
+                    Value = value;
+                IsSelectedFlange.Value = isSelected;
+            }
+            else
+            if (ImGuiNET.ImGui.MenuItem(Context.GetLabel(this, Label), Shortcut, selected: false, enabled: Enabled))
                 Value = value;
+
         }
     }
 }
