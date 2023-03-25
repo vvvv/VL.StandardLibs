27aaa2d9bf1fb061244cf641c113ad971be701cf
joreg <joreg@vvvv.org>
2023-03-25 01:03:52 +0100

    avoid AV when CopyMirror is true

--- VL.Stride.Runtime/src/Rendering/VLForwardRenderer.cs
+++ VL.Stride.Runtime/src/Rendering/VLForwardRenderer.cs
@@ -898,7 +898,7 @@ namespace VL.Stride.Rendering
                     }
 
                     //draw mirror to backbuffer (if size is matching and full viewport)
-                    if (VRSettings.CopyMirror)
+                    if (VRSettings.CopyMirror && VRSettings.VRDevice.MirrorTexture != null) //remove check for mirrortexture here, once OpenXR has one
                     {
                         CopyOrScaleTexture(drawContext, VRSettings.VRDevice.MirrorTexture, drawContext.CommandList.RenderTarget);
                     }
