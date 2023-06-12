309783486602c8a672c0780eea320298895dfc3d
Johannes Schmidt <schmidt@kopffarben.de>
2023-06-12 21:48:28 +0200

    WIP first working transactional SetString with Result

--- VL.IO.Redis/src/RedisExtensions.cs
+++ VL.IO.Redis/src/RedisExtensions.cs
@@ -14,25 +14,63 @@ namespace VL.IO.Redis
     {
         private readonly ITransaction _tran;
         private readonly IList<Task<object>> _tasks = new List<Task<object>>();
+        private Task<bool> _successfulTask;
+        private bool _successful;
+        private Task<object[]> _result;
 
         public RedisCommandQueue Enqueue(Func<ITransaction, Task<object>> cmd)
         {
-            _tasks.Add(cmd(_tran));
+            if (_tran != null)
+                _tasks.Add(cmd(_tran));
             return this;
         }
 
         public RedisCommandQueue(ITransaction tran) => _tran = tran;
+
+        public void ExecuteAsync()
+        {
+            _successfulTask = _tran.ExecuteAsync();
+        }
+
+        public bool AwaitExecute(int timeout = 16)
+        {
+            if (_successfulTask.Wait(timeout))
+            {
+                _result = Task.WhenAll(_tasks);
+                _successful = true;
+                return true;
+            }
+            else 
+            {
+                _successful = false;
+                return false; 
+            }
+        }
+
+        public object[] Result(int timeout = 16)
+        {
+            if (_successful)
+                if (_result.Wait(timeout))
+                    return _result.Result;
+            return new object[0];
+        }
 
         public async Task<object[]> Execute()
         {
             if (await _tran.ExecuteAsync())
                 return await Task.WhenAll(_tasks);
             else
-                return null;
+                return new object[0];
         }
     }
     public static class RedisExtensions
     {
+
+        public static Task<object> Cast<T>(this Task<T> task)
+        {
+            return task.ContinueWith(t => (object)t.Result);
+        }
+
         public static IObservable<RedisValue> WhenMessageReceived(this ISubscriber subscriber, RedisChannel channel)
         {
             return Observable.Create<RedisValue>(async (obs, ct) =>
