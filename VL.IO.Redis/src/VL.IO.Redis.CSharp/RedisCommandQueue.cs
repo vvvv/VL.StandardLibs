46d223a5213fb41e907821df7d990e6b32e840b5
Johannes Schmidt <schmidt@kopffarben.de>
2023-08-16 12:31:47 +0200

    Rebuild Observable Structure

--- VL.IO.Redis/src/VL.IO.Redis.CSharp/RedisCommandQueue.cs
+++ VL.IO.Redis/src/VL.IO.Redis.CSharp/RedisCommandQueue.cs
@@ -10,139 +10,28 @@ using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Transactions;
+using VL.Lib.Collections;
 
 namespace VL.IO.Redis
 {
     public record RedisCommandQueue
     {
-        private ITransaction _tran;
-        private IList<Task<KeyValuePair<Guid, object>>> _tasks;
-        private ConcurrentQueue<Func<ITransaction, Task<KeyValuePair<Guid, object>>>> _cmds;
-        private List<RedisKey> _changedkeys;
+        internal ITransaction _tran;
+        internal IList<Task<KeyValuePair<Guid, object>>> _tasks;
+        internal ConcurrentQueue<Func<ITransaction, Task<KeyValuePair<Guid, object>>>> _cmds;
+        internal SpreadBuilder<string> _changes;
 
-        public void Enqueue(Func<ITransaction, Task<KeyValuePair<Guid, object>>> cmd )
-        {
-            _cmds.Enqueue(cmd);
-        }
 
-        public void ChangedKeys(IEnumerable<RedisKey> keys)
-        {
-            _changedkeys.AddRange(keys);
-        }
 
         public RedisCommandQueue(IDatabase database)
         {
             _tran           = database.CreateTransaction();
             _tasks          = new List<Task<KeyValuePair<Guid, object>>>();
             _cmds           = new ConcurrentQueue<Func<ITransaction, Task<KeyValuePair<Guid, object>>>>();
-            _changedkeys    = new List<RedisKey>();
+            _changes        = new SpreadBuilder<string>();
         }
 
-        public static IObservable<RedisCommandQueue> ApplyTransactions(IObservable<RedisCommandQueue> observable, Action<float, int, ImmutableList<RedisKey>> action)
-        {
-            return Observable.Create<RedisCommandQueue>((obs) =>
-            {
-                var syncObs = Observer.Synchronize(obs, true);
-                return observable.Subscribe(
-                (queue) =>
-                {
-                    try
-                    {
-                        var sw = Stopwatch.StartNew();
-
-                        if (queue._tran != null)
-                        {
-                            foreach (var cmd in queue._cmds)
-                            {
-                                queue._tasks.Add(cmd(queue._tran));
-                            }
-                            action.Invoke((float)sw.ElapsedTicks / (float)(TimeSpan.TicksPerMillisecond), queue._tasks.Count, queue._changedkeys.ToImmutableList());
-
-                            syncObs.OnNext(queue);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex);
-                        syncObs.OnError(ex);
-                    }
-                },
-                (ex) =>
-                {
-                    Console.WriteLine(ex);
-                    syncObs.OnError(ex);
-                },
-                () =>
-                {
-                    Console.WriteLine("COMPLETED");
-                    syncObs.OnCompleted();
-                });
-            });
-        }
-
-
-        public static IObservable<ImmutableDictionary<Guid, object>> ExecuteTransaction(IObservable<RedisCommandQueue> observable, Action<float> action, System.Reactive.Concurrency.IScheduler scheduler)
-        {
-
-            return Observable.Create<ImmutableDictionary<Guid, object>>((obs) =>
-            {
-                //var syncObs = Observer.Synchronize(obs, true);
-                var syncObs = Observer.NotifyOn(obs, scheduler);
-
-                return observable.Subscribe(async 
-                    // onNext
-                    (queue) =>
-                    {
-                        try
-                        {
-                            var sw = Stopwatch.StartNew();
-
-                            if (await queue._tran.ExecuteAsync())
-                            {
-                                try
-                                {
-                                    var resultAwaiter = Task.WhenAll(queue._tasks).GetAwaiter();
-
-                                    resultAwaiter.OnCompleted(() =>
-                                    {
-                                        action.Invoke((float)sw.ElapsedTicks / (float)(TimeSpan.TicksPerMillisecond));
-
-                                        ImmutableDictionary<Guid, object>.Builder builder = ImmutableDictionary.CreateBuilder<Guid, object>();
-                                        foreach (var kv in resultAwaiter.GetResult())
-                                        {
-                                            builder.TryAdd(kv.Key, kv.Value);
-                                        }
-                                        syncObs.OnNext(builder.ToImmutable());
-                                    });
-                                }
-                                catch (Exception ex) 
-                                {
-                                    Console.WriteLine(ex);
-                                    syncObs.OnError(ex);
-                                }
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex);
-                            syncObs.OnError(ex);
-                        }
-                    }
-                    // onError
-                    ,(ex) => 
-                    {
-                        Console.WriteLine(ex);
-                        syncObs.OnError(ex); 
-                    }
-                    // onComplete
-                    ,() =>
-                    {
-                        Console.WriteLine("COMPLETED");
-                        syncObs.OnCompleted();
-                    }
-                );
-            });
-        }
+        
 
     }
 }
--- VL.IO.Redis/src/VL.IO.Redis.CSharp/RedisExtensions.cs
+++ VL.IO.Redis/src/VL.IO.Redis.CSharp/RedisExtensions.cs
@@ -11,6 +11,11 @@ using System.Collections.Immutable;
 using System.Reactive.Concurrency;
 using System.Threading.Channels;
 using System.Transactions;
+using System.Diagnostics;
+using VL.Core;
+using System.Security.Cryptography;
+using VL.Lib.Collections;
+using System.Reflection.Metadata.Ecma335;
 
 namespace VL.IO.Redis
 {
@@ -20,33 +25,181 @@ namespace VL.IO.Redis
         public ThreadSafeToggle() { }
 
         private bool enabled = true;
-        //private object syncObj = new object();
+        private object syncObj = new object();
 
         public void Enable()
         {
-            //lock (syncObj)
+            lock (syncObj)
             {
                 enabled = true;
             }
         }
         public void Disable()
         {
-            //lock (syncObj)
+            lock (syncObj)
             {
                 enabled = false;
             }
         }
         public bool Enabled()
         {
-            //lock (syncObj)
+            lock (syncObj)
             {
                 return enabled;
             }
         }
     }
 
+    public class Transaction
+    {
+
+    }
+
     public static class RedisExtensions
     {
+        /// <summary>
+        /// xs --x---x---x---x-------x---x---x-
+        ///       \           \       \        
+        /// ys ----y-----------y---y---y-------
+        ///        |           |       |       
+        /// rs ----x-----------x-------x-------
+        ///        y           y       y       
+        /// </summary>
+        /// <typeparam name="TLeft"></typeparam>
+        /// <typeparam name="TRight"></typeparam>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static IObservable<ValueTuple<TRight,TLeft>> WithLatestWhenNew<TLeft, TRight>(this IObservable<TLeft> left, IObservable<TRight> right)
+        {
+            return left.Join(
+                right,
+                l => left.Any().Merge(right.Any()),
+                r => Observable.Empty<Unit>(),
+                (l, r) => { return ValueTuple.Create(r, l); });
+
+        }
+
+        public static ValueTuple<RedisCommandQueue, TInput> Enqueue<TInput,TOutput>(ValueTuple<RedisCommandQueue,TInput> input, Func<ITransaction, TInput, Task<TOutput>> cmd, Guid guid, Optional<Func<TInput,IEnumerable<string>>> keys)
+        {
+            input.Item1._cmds.Enqueue((tran) => cmd.Invoke(tran, input.Item2).ContinueWith(t => new KeyValuePair<Guid, object>(guid, (object)t.Result)));
+
+            if (keys.HasValue)
+            {
+                input.Item1._changes.AddRange(keys.Value.Invoke(input.Item2));
+     
+            }
+
+            return input;
+        }
+
+        public static IObservable<RedisCommandQueue> ApplyTransactions(this IObservable<RedisCommandQueue> observable, Action<float, int> action, Func<Spread<string>,RedisValue> serializeChanges, Guid guid)
+        {
+            return Observable.Create<RedisCommandQueue>((obs) =>
+            {
+                var syncObs = Observer.Synchronize(obs, true);
+                return observable.Subscribe(
+                (queue) =>
+                {
+                    try
+                    {
+                        var sw = Stopwatch.StartNew();
+
+                        if (queue._tran != null)
+                        {
+                            queue._tasks.Add(queue._tran.PublishAsync("Changed", serializeChanges.Invoke(queue._changes.ToSpread()) ).ContinueWith(t => new KeyValuePair<Guid, object>(guid, (object)t.Result)));
+
+                            foreach (var cmd in queue._cmds)
+                            {
+                                queue._tasks.Add(cmd(queue._tran));
+                            }
+                            action.Invoke((float)sw.ElapsedTicks / (float)(TimeSpan.TicksPerMillisecond), queue._tasks.Count);
+
+                            syncObs.OnNext(queue);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                        syncObs.OnError(ex);
+                    }
+                },
+                (ex) =>
+                {
+                    Console.WriteLine(ex);
+                    syncObs.OnError(ex);
+                },
+                () =>
+                {
+                    Console.WriteLine("COMPLETED");
+                    syncObs.OnCompleted();
+                });
+            });
+        }
+
+
+        public static IObservable<ImmutableDictionary<Guid, object>> ExecuteTransaction(this IObservable<RedisCommandQueue> observable, Action<float> action, IScheduler scheduler)
+        {
+
+            return Observable.Create<ImmutableDictionary<Guid, object>>((obs) =>
+            {
+                //var syncObs = Observer.Synchronize(obs, true);
+                var syncObs = Observer.NotifyOn(obs, scheduler);
+
+                return observable.Subscribe(async
+                    // onNext
+                    (queue) =>
+                {
+                    try
+                    {
+                        var sw = Stopwatch.StartNew();
+
+                        if (await queue._tran.ExecuteAsync())
+                        {
+                            try
+                            {
+                                var resultAwaiter = Task.WhenAll(queue._tasks).GetAwaiter();
+
+                                resultAwaiter.OnCompleted(() =>
+                                {
+                                    action.Invoke((float)sw.ElapsedTicks / (float)(TimeSpan.TicksPerMillisecond));
+
+                                    ImmutableDictionary<Guid, object>.Builder builder = ImmutableDictionary.CreateBuilder<Guid, object>();
+                                    foreach (var kv in resultAwaiter.GetResult())
+                                    {
+                                        builder.TryAdd(kv.Key, kv.Value);
+                                    }
+                                    syncObs.OnNext(builder.ToImmutable());
+                                });
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex);
+                                syncObs.OnError(ex);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                        syncObs.OnError(ex);
+                    }
+                }
+                    // onError
+                    , (ex) =>
+                    {
+                        Console.WriteLine(ex);
+                        syncObs.OnError(ex);
+                    }
+                    // onComplete
+                    , () =>
+                    {
+                        Console.WriteLine("COMPLETED");
+                        syncObs.OnCompleted();
+                    }
+                );
+            });
+        }
 
         public static Task<KeyValuePair<Guid, object>> Cast<T>(this Task<T> task, Guid guid)
         {
