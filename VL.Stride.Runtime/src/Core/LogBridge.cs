8cc3e8ea324b1809b58e10627772b6d89375812d
Elias Holzer <elias@vvvv.org>
2023-10-26 19:02:48 +0200

    Adds a logging bridge to Stride
    Good way to test is creating syntax errors in a shader. Because internally a bunch of systems try to compile the shader again and again when in error state one gets overwhelmed with the same messages immediately.

---
+++ VL.Stride.Runtime/src/Core/LogBridge.cs
@@ -0,0 +1,63 @@
+﻿#nullable enable
+using Microsoft.Extensions.Logging;
+using Stride.Core.Diagnostics;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+namespace VL.Stride.Core
+{
+    sealed class LogBridge : LogListener
+    {
+        private readonly ILoggerFactory loggerFactory;
+        private readonly ILogger logger;
+
+        private string? lastModule;
+        private ILogger? lastLogger;
+
+        public LogBridge(ILoggerFactory loggerFactory, ILogger logger)
+        {
+            this.loggerFactory = loggerFactory;
+            this.logger = logger;
+        }
+
+        private ILogger GetLogger(string? module)
+        {
+            if (module is null)
+                return logger;
+
+            if (module == lastModule)
+                return lastLogger!;
+
+            lastModule = module;
+            return lastLogger = loggerFactory.CreateLogger(module);
+        }
+
+        protected override void OnLog(ILogMessage logMessage)
+        {
+            var logger = GetLogger(logMessage.Module);
+            var logLevel = ToLogLevel(logMessage.Type);
+            if (logMessage is global::Stride.Core.Diagnostics.LogMessage logMessage2)
+                logger.Log(logLevel, logMessage2.Exception, logMessage2.Text);
+            else
+                logger.Log(logLevel, logMessage.Text);
+        }
+
+        static LogLevel ToLogLevel(LogMessageType logMessageType)
+        {
+            switch (logMessageType)
+            {
+                case LogMessageType.Fatal:
+                    return LogLevel.Critical;
+                case LogMessageType.Error:
+                    return LogLevel.Error;
+                case LogMessageType.Warning:
+                    return LogLevel.Warning;
+                case LogMessageType.Info:
+                    return LogLevel.Information;
+                case LogMessageType.Debug:
+                    return LogLevel.Debug;
+                default:
+                    return LogLevel.Trace;
+            }
+        }
+    }
+}
