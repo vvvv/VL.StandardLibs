76544e78c5a7ebf67a2f079a0850c299bca9ead8
joreg <joreg@vvvv.org>
2023-11-08 18:35:10 +0100

    LogMessage.Exception is now null by default

--- VL.CoreLib/src/Logging/Extensions.cs
+++ VL.CoreLib/src/Logging/Extensions.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.Extensions.Logging;
+﻿#nullable enable
+using Microsoft.Extensions.Logging;
+using System;
 using VL.Lib.Collections;
 
 namespace VL.Lib.Logging
 {
     public static class VLLoggerExtensions
     {
-        public static void Log(this ILogger logger, LogLevel logLevel, EventId eventId, System.Exception exception, string message, Spread<object> args)
+        public static void Log(this ILogger logger, LogLevel logLevel, EventId eventId, string message, Spread<object> args, Exception? exception = null)
         {
             logger.Log(logLevel, eventId, exception, message, args.GetInternalArray());
         }
