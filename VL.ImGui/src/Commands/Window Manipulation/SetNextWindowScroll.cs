b5aeacfeb1b6c0458c6e7e6b40ba6a63bb9a940e
Elias Holzer <elias@vvvv.org>
2023-11-09 15:31:30 +0100

    [ImGui] Exposes `SetNextWindowScroll`

---
+++ VL.ImGui/src/Commands/Window Manipulation/SetNextWindowScroll.cs
@@ -0,0 +1,18 @@
+﻿using Stride.Core.Mathematics;
+
+namespace VL.ImGui.Widgets
+{
+    [GenerateNode(Category = "ImGui.Commands", GenerateRetained = false, IsStylable = false)]
+    internal partial class SetNextWindowScroll : Widget
+    {
+        public Vector2 Scroll { private get; set; }
+
+        public bool Enabled { private get; set; } = true;
+
+        internal override void UpdateCore(Context context)
+        {
+            if (Enabled)
+                ImGuiNET.ImGui.SetNextWindowScroll(Scroll.FromHectoToImGui());
+        }
+    }
+}
