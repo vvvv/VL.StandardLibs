e54942b43dc70b0540e1c64515bd37b7892dac7b
Elias Holzer <elias@vvvv.org>
2023-11-11 20:04:21 +0100

    Moves FSPickler into its own package (needs commit in private repo!)

---
+++ VL.Serialization.FSPickler/src/FSPicklerSerialization.cs
@@ -0,0 +1,59 @@
+﻿using MBrace.FsPickler;
+using MBrace.FsPickler.Json;
+using CommunityToolkit.HighPerformance;
+using System;
+using System.Collections.Generic;
+using Microsoft.FSharp.Core;
+using VL.Core;
+
+namespace VL.Serialization.FSPickler
+{
+    public static class FSPicklerSerialization
+    {
+        static FSharpOption<ITypeNameConverter> TypeNameConverter => new FSharpOption<ITypeNameConverter>(AppHost.CurrentOrGlobal.Services.GetOrAddService<ITypeNameConverter>(s => new FSPickler.TypeNameConverter(s.GetRequiredService<TypeRegistry>())));
+        static FSharpOption<IPicklerResolver> PicklerResolver => new FSharpOption<IPicklerResolver>(FSPickler.PicklerResolver.Instance);
+
+        public static string SerializeXml<T>(T value, bool indent = false)
+        {
+            var serializer = new XmlSerializer(indent, TypeNameConverter, PicklerResolver);
+            return serializer.PickleToString(value);
+        }
+
+        public static T DeserializeXml<T>(string serializedValue, bool indent = false)
+        {
+            var serializer = new XmlSerializer(indent, TypeNameConverter, PicklerResolver);
+            return serializer.UnPickleOfString<T>(serializedValue);
+        }
+
+        public static string SerializeJson<T>(T value, bool indent = false, bool omitHeader = true)
+        {
+            var serializer = new JsonSerializer(indent, omitHeader, TypeNameConverter, PicklerResolver);
+            return serializer.PickleToString(value);
+        }
+
+        public static T DeserializeJson<T>(string serializedValue, bool indent = false, bool omitHeader = true)
+        {
+            var serializer = new JsonSerializer(indent, omitHeader, TypeNameConverter, PicklerResolver);
+            return serializer.UnPickleOfString<T>(serializedValue);
+        }
+
+        public static byte[] SerializeBinary<T>(T value, bool forceLittleEndian = false)
+        {
+            var serializer = new BinarySerializer(forceLittleEndian, TypeNameConverter, PicklerResolver);
+            return serializer.Pickle(value);
+        }
+
+        public static T DeserializeBinary<T>(IEnumerable<byte> serializedValue, bool forceLittleEndian = false)
+        {
+            if (serializedValue.TryGetMemory(out var memory))
+            {
+                var stream = memory.AsStream();
+                var serializer = new BinarySerializer(forceLittleEndian, TypeNameConverter, PicklerResolver);
+                return serializer.Deserialize<T>(stream);
+            }
+            throw new ArgumentException($"Couldn't retrieve memory from {typeof(T)}", nameof(serializedValue));
+        }
+
+
+    }
+}
---
+++ VL.Serialization.FSPickler/src/TypeNameConverter.cs
@@ -0,0 +1,49 @@
+﻿using MBrace.FsPickler;
+using VL.Core;
+
+namespace VL.Serialization.FSPickler
+{
+
+    sealed class TypeNameConverter : ITypeNameConverter
+    {
+        private readonly TypeRegistry typeRegistry;
+
+        public TypeNameConverter(TypeRegistry typeRegistry)
+        {
+            this.typeRegistry = typeRegistry;
+        }
+
+        public TypeInfo OfSerializedType(TypeInfo value)
+        {
+            const string proxySuffix = "+Proxy";
+
+            var name = value.Name;
+            if (name.EndsWith(proxySuffix))
+                name = name.Substring(0, name.Length - proxySuffix.Length);
+
+            foreach (var typeInfo in typeRegistry.RegisteredTypes)
+            {
+                if (typeInfo.ClrType.FullName == name)
+                    return new TypeInfo(ToFullName(typeInfo), assemblyInfo: value.AssemblyInfo);
+            }
+            return value;
+        }
+
+        public TypeInfo ToDeserializedType(TypeInfo value)
+        {
+            foreach (var typeInfo in typeRegistry.RegisteredTypes)
+            {
+                if (ToFullName(typeInfo) == value.Name)
+                    return new TypeInfo(typeInfo.ClrType.FullName, AssemblyInfo.OfAssembly(typeInfo.ClrType.Assembly));
+            }
+            return value;
+        }
+
+        static string ToFullName(IVLTypeInfo typeInfo)
+        {
+            if (string.IsNullOrWhiteSpace(typeInfo.Category))
+                return typeInfo.Name;
+            return $"{typeInfo.Category}.{typeInfo.Name}";
+        }
+    }
+}
