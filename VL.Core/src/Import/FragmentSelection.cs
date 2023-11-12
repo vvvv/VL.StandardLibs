ecd8ed96e5c64c4dec84e3f2fbff656faa227d90
Elias Holzer <elias@vvvv.org>
2023-11-12 18:46:42 +0100

    Adds new attributes to import C# defined types and nodes as is

---
+++ VL.Core/src/Import/FragmentSelection.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+
+namespace VL.Core.Import
+{
+    /// <summary>
+    /// Allows to specifify what members of a type get selected to form a process its fragments.
+    /// </summary>
+    public enum FragmentSelection
+    {
+        /// <summary>
+        /// All public methods and properties of the type will be included.
+        /// </summary>
+        Implicit,
+        /// <summary>
+        /// Only public methods and properties with the <see cref="FragmentAttribute"/> will be included.
+        /// </summary>
+        Explicit
+    }
+}
