vvvv/VL.StandardLibs

commit 7251fc41cbe32dadae2a1d4037435926a6cf6e07

(azeno): fixes non-deterministic behavior of some stride material nodes. Copy on write didn't work as Stride didn't implement equality for their types properly (e.g. for MaterialSpecularThinGlassModelFeature). Using ReferenceEquality now.

VL.Stride.Runtime/src/_NodeFactory_/CustomNode.cs
@@ -499,13 +499,16 @@ namespace VL.Stride
 
         class CachedInputPin<T> : InputPin<T>, IVLPin
         {
+            private static bool ReferenceEquals(T instance1, T instance2) => object.ReferenceEquals(instance1, instance2);
+
             readonly Func<T, T, bool> equals;
             T lastValue;
 
             public CachedInputPin(Node node, TInstance instance, Func<TInstance, T> getter, Action<TInstance, T> setter, T initialValue, Func<T, T, bool> equals = default) 
                 : base(node, instance, getter, setter, initialValue)
             {
-                this.equals = equals ?? EqualityComparer<T>.Default.Equals;
+                // Stride object equality is buggy! For example of type  MaterialSpecularThinGlassModelFeature
+                this.equals = equals ?? (typeof(T).IsValueType ? EqualityComparer<T>.Default.Equals : ReferenceEquals);
                 lastValue = initialValue;
             }
 

commit 93290edc461b75f4b069e345fb970fe5b3a23279

[Stride] Fixes regression introduced in 7251fc41cbe32dadae2a1d4037435926a6cf6e07
Reference equality was also used for strings which in turn caused the whole render pipeline to get re-created every frame.

VL.Stride.Runtime/src/_NodeFactory_/CustomNode.cs
@@ -1,5 +1,6 @@
 ﻿using Stride.Core.Collections;
 using Stride.Engine;
+using Stride.Rendering.Materials;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
@@ -508,7 +509,7 @@ namespace VL.Stride
                 : base(node, instance, getter, setter, initialValue)
             {
                 // Stride object equality is buggy! For example of type  MaterialSpecularThinGlassModelFeature
-                this.equals = equals ?? (typeof(T).IsValueType ? EqualityComparer<T>.Default.Equals : ReferenceEquals);
+                this.equals = equals ?? (typeof(T).IsAssignableTo(typeof(IMaterialShaderGenerator)) ? ReferenceEquals : EqualityComparer<T>.Default.Equals);
                 lastValue = initialValue;
             }
 