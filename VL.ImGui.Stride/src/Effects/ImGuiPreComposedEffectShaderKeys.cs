8cd3d46932506d84682385ee262ca8c47cacd4aa
Sebastian Gregor <gregor@vvvv.org>
2024-03-08 20:26:09 +0100

    experiments with the idea of pre-composition mentioned here: https://github.com/vvvv/VL.StandardLibs/issues/669#issuecomment-1986010584

---
+++ VL.ImGui.Stride/src/Effects/ImGuiPreComposedEffectShaderKeys.cs
@@ -0,0 +1,9 @@
+﻿using Stride.Rendering;
+
+namespace VL.ImGui.Stride.Effects
+{
+    internal partial class ImGuiPreComposedEffectShaderKeys
+    {
+        public static readonly PermutationParameterKey<bool> ColorIsSRgb = ParameterKeys.NewPermutation(false);
+    }
+}
---
+++ VL.ImGui.Stride/src/ImGuiPreComposedRenderer.InputHandling.cs
@@ -0,0 +1,29 @@
+﻿using Stride.Input;
+using Stride.Rendering;
+using System.Reactive.Disposables;
+using VL.Stride;
+
+namespace VL.ImGui
+{
+    partial class ImGuiPreComposedRenderer
+    {
+        IDisposable SubscribeToInputSource(IInputSource inputSource, RenderDrawContext context)
+        {
+            if (inputSource is null)
+                return Disposable.Empty;
+
+            var inputManager = context.RenderContext.Services.GetService<InputManager>();
+            if (inputManager is null)
+                return Disposable.Empty;
+
+            return inputManager.GetNotifications(inputSource, sender: this)
+                .Subscribe(notification =>
+                {
+                    using (_context.MakeCurrent())
+                    {
+                        _io.HandleNotification(notification);
+                    }
+                });
+        }
+    }
+}
