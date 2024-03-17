ed5188e2844346f2f4d1c02e291ffb9edb87221f
Elias Holzer <elias@vvvv.org>
2024-03-17 13:24:09 +0100

    Monadic value rework
    - Replaces the `IMonadicFactory` and `IMonadBuilder` interfaces with a new `IMonadicValue`
    - Marks `Nullable<T>` as immutable same as `Optional<T>`
    - Adds object editor which handles `Nullable<T>`, `Optional<T>` and `IMonadicValue<T>` (`IChannel<T>`, `GPU<T>`)
    - These are breaking changes for FUSE which will be dealt with in a separate PR

---
+++ VL.Core/src/MonadicUtils.cs
@@ -0,0 +1,95 @@
+﻿#nullable enable
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace VL.Core
+{
+    public static class MonadicUtils
+    {
+        public static bool ImplementsMonadicValue(this Type type)
+        {
+            return type.IsGenericType && type.IsAssignableTo(typeof(IMonadicValue));
+        }
+
+        public static bool IsOptional(this Type type)
+        {
+            return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Optional<>);
+        }
+
+        public static bool IsNullable(this Type type)
+        {
+            return Nullable.GetUnderlyingType(type) != null;
+        }
+
+        internal static bool HasMonadicValueEditor(this Type type)
+        {
+            return type.ImplementsMonadicValue() || type.IsOptional() || type.IsNullable();
+        }
+
+        internal static IMonadicValueEditor<TValue, TMonad>? GetMonadicEditor<TValue, TMonad>()
+        {
+            if (typeof(TMonad).IsAssignableTo(typeof(IMonadicValue)))
+                return Activator.CreateInstance(typeof(MonadicValueEditor<,>).MakeGenericType(typeof(TValue), typeof(TMonad))) as IMonadicValueEditor<TValue, TMonad>;
+
+            if (typeof(TMonad).IsOptional())
+                return Activator.CreateInstance(typeof(OptionalEditor<TValue>)) as IMonadicValueEditor<TValue, TMonad>;
+
+            if (typeof(TMonad).IsNullable())
+                return Activator.CreateInstance(typeof(NullableEditor<>).MakeGenericType(typeof(TValue))) as IMonadicValueEditor<TValue, TMonad>;
+
+            return null;
+        }
+
+        public static TMonad Create<TMonad, TValue>(NodeContext nodeContext, TValue? value) 
+            where TMonad : IMonadicValue<TValue>
+        {
+            return (TMonad)TMonad.Create(nodeContext, value);
+        }
+
+        private sealed class OptionalEditor<T> : IMonadicValueEditor<T, Optional<T>>
+        {
+            public Optional<T> Create(T? value) => new Optional<T>(value!);
+
+            public T? GetValue(Optional<T> optional) => optional.Value;
+
+            public bool HasValue(Optional<T> optional) => optional.HasValue;
+
+            public Optional<T> SetValue(Optional<T> _, T? value) => new Optional<T>(value!);
+        }
+
+        private sealed class NullableEditor<T> : IMonadicValueEditor<T, T?>
+            where T : struct
+        {
+            public T? Create(T value) => value;
+            public T GetValue(T? nullable) => nullable!.Value;
+            public bool HasValue([NotNullWhen(true)] T? nullable) => nullable.HasValue;
+            public T? SetValue(T? _, T value) => new T?(value);
+        }
+
+        private sealed class MonadicValueEditor<TValue, TMonad> : IMonadicValueEditor<TValue, TMonad>
+            where TMonad : IMonadicValue<TValue>
+        {
+            public TMonad Create(TValue? value)
+            {
+                return MonadicUtils.Create<TMonad, TValue>(NodeContext.CurrentRoot, value);
+            }
+
+            public TValue? GetValue(TMonad monad) => monad.Value;
+            public bool HasValue(TMonad? monad) => monad is not null && monad.HasValue;
+            public TMonad SetValue(TMonad monad, TValue? value)
+            {
+                if (monad.AcceptsValue)
+                {
+                    return (TMonad)monad.SetValue(value);
+                }
+                else
+                {
+                    return Create(value);
+                }
+            }
+
+            public bool HasCustomDefault => TMonad.HasCustomDefault;
+        }
+    }
+}
--- VL.Core/src/MonadicValues.cs
+++ VL.Core/src/MonadicValues.cs
@@ -1,37 +1,29 @@
 ﻿#nullable enable
 using System;
-using System.Reflection;
+using System.Diagnostics.CodeAnalysis;
 using VL.Lib.IO;
 
 namespace VL.Core
 {
     /// <summary>
-    /// Marks a type as monadic - it can wrap any basic type. The value wrapping is done through an intermediate as specified by the given factory.
+    /// Allows to specify a type filter for a <see cref="IMonadicValue{TValue}"/>.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface, AllowMultiple = false, Inherited = false)]
-    public class MonadicAttribute : Attribute
+    public class MonadicTypeFilterAttribute : Attribute
     {
-        public Type Factory { get; }
-
         /// <summary>
-        /// An optional type filter. The type must have a default constructor and implement <see cref="IMonadicTypeFilter"/>.
+        /// The type must have a default constructor and implement <see cref="IMonadicTypeFilter"/>.
         /// </summary>
-        public Type? TypeFilter { get; }
+        public Type TypeFilter { get; }
 
-        public MonadicAttribute(Type factory)
+        public MonadicTypeFilterAttribute(Type typeFilter)
         {
-            Factory = factory;
-        }
-
-        public MonadicAttribute(Type factory, Type typeFilter)
-        {
-            Factory = factory;
             TypeFilter = typeFilter;
         }
     }
 
     /// <summary>
-    /// Allows to define what types the factory accepts.
+    /// Defines what types shall be accepted by a <see cref="IMonadicValue{TValue}"/>.
     /// </summary>
     public interface IMonadicTypeFilter
     {
@@ -61,40 +53,57 @@ namespace VL.Core
     }
 
     /// <summary>
-    /// Implementations must have a default constructor as well as a static readonly field called "Default".
+    /// Non-generic marker interface. Do not implement directly.
     /// </summary>
-    /// <typeparam name="TValue">The type of the value</typeparam>
-    /// <typeparam name="TMonad">The type of the monadic value</typeparam>
-    public interface IMonadicFactory<TValue, TMonad>
+    public interface IMonadicValue
     {
-        /// <summary>
-        /// Creates a monad builder.
-        /// </summary>
-        /// <param name="isConstant">Whether or not the value is constant.</param>
-        /// <returns>The monad builder</returns>
-        IMonadBuilder<TValue, TMonad> GetMonadBuilder(bool isConstant);
-
-        /// <summary>
-        /// Creates a monad builder.
-        /// </summary>
-        /// <param name="isConstant">Whether or not the value is constant.</param>
-        /// <param name="nodeContext">The node context - its path will have the id of the sink appended to it.</param>
-        /// <returns>The monad builder</returns>
-        IMonadBuilder<TValue, TMonad> GetMonadBuilder(bool isConstant, NodeContext nodeContext) => GetMonadBuilder(isConstant);
     }
 
     /// <summary>
-    /// Builds the monadic value <typeparamref name="TMonad"/> out of <typeparamref name="TValue"/>.
+    /// Tells VL that connections from <typeparamref name="TValue"/> to the type implementing this interface are allowed.
     /// </summary>
-    /// <typeparam name="TValue">The type of the value</typeparam>
-    /// <typeparam name="TMonad">The type of the monadic value</typeparam>
-    public interface IMonadBuilder<TValue, TMonad>
+    public interface IMonadicValue<TValue> : IMonadicValue
     {
-        TMonad Return(TValue value);
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        static abstract IMonadicValue<TValue> Create(NodeContext nodeContext, TValue? value);
 
         /// <summary>
-        /// Called when the system has no value yet. This is usually true for unconnected input pins.
+        /// Whether or not the default value is not NULL. This is a hint for UIs to not display the checkbox.
         /// </summary>
-        TMonad? Default() => default;
+        static virtual bool HasCustomDefault => false;
+
+        /// <summary>
+        /// Whether or not a value is available.
+        /// </summary>
+        bool HasValue { get; }
+
+        /// <summary>
+        /// Whether or not a value can be assigned to this instance.
+        /// </summary>
+        bool AcceptsValue { get; }
+
+        /// <summary>
+        /// The stored value.
+        /// </summary>
+        TValue? Value { get; }
+
+        /// <summary>
+        /// Sets a new value and potentially returns a new instance.
+        /// </summary>
+        IMonadicValue<TValue> SetValue(TValue? value);
+    }
+
+    /// <summary>
+    /// Utility interface to share common editor implementations for <see cref="Nullable{T}"/>, <see cref="Optional{T}"/> and <see cref="IMonadicValue{TValue}"/>.
+    /// </summary>
+    internal interface IMonadicValueEditor<TValue, TMonad>
+    {
+        TMonad Create(TValue? value);
+        bool HasValue([NotNullWhen(true)] TMonad? monad);
+        TValue? GetValue(TMonad monad);
+        TMonad SetValue(TMonad monad, TValue? value);
+        bool HasCustomDefault => false;
     }
 }
