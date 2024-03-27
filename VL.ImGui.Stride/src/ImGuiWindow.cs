74d07a362a954ef24d4658638736917971343af7
Johannes Schmidt <schmidt@kopffarben.de>
2024-03-27 10:47:26 +0100

    WIP

---
+++ VL.ImGui.Stride/src/ImGuiWindow.cs
@@ -0,0 +1,171 @@
+﻿
+
+using Stride.Core.Mathematics;
+using System.Runtime.InteropServices;
+
+using Stride.Games;
+using Stride.Graphics;
+
+using VL.Core;
+using VL.Stride;
+using VL.Stride.Games;
+using ImGuiNET;
+using Stride.Input;
+using Stride.Rendering;
+using VL.Stride.Rendering;
+using Stride.Engine;
+using VL.Lib.Collections;
+using VL.Stride.Engine;
+using Stride.Rendering.Compositing;
+
+namespace VL.ImGui.Stride
+{
+    using GameWindowRenderer = VL.Stride.Games.GameWindowRenderer;
+
+    internal class ImGuiWindow : IDisposable
+    {
+        private readonly GCHandle _gcHandle;
+        private readonly SchedulerSystem _schedulerSystem;
+        private readonly GameWindowRenderer _gameWindowRenderer;
+        private readonly IInputSource _inputSource;
+        private readonly WindowRenderer _windowRenderer;
+
+        public IntPtr Handle => _gameWindowRenderer.Window.NativeWindow.Handle;
+
+        public Int2 Position
+        {
+            get { return _gameWindowRenderer.Window.Position; }
+            set { _gameWindowRenderer.Window.Position = value; }
+        }
+
+        public Int2 Size
+        {
+            get { return new Int2(_gameWindowRenderer.Window.ClientBounds.Width, _gameWindowRenderer.Window.ClientBounds.Height); }
+            set { _gameWindowRenderer.Window.SetSize(value); }
+        }
+
+        public string Title
+        {
+            get { return _gameWindowRenderer.Window.Title; }
+            set { _gameWindowRenderer.Window.Title = value; }
+        }
+
+        public bool IsActivated
+        {
+            get { return _gameWindowRenderer.Window.IsActivated; }
+        }
+
+        public void Activate()
+        {
+            _gameWindowRenderer.Window.BringToFront();
+        }
+
+        public bool IsMinimized
+        {
+            get { return _gameWindowRenderer.Window.IsMinimized; }
+        }
+
+        public ImGuiWindow(NodeContext nodeContext, ImGuiViewportPtr vp)
+        {
+            _gcHandle = GCHandle.Alloc(this);
+            
+
+            using (var gameHandle = nodeContext.AppHost.Services.GetGameHandle())
+            {
+                var registry = gameHandle.Resource.Services;
+                var gameContext = GameContextFactory.NewGameContextSDL(640, 480, true);
+
+                _inputSource = InputSourceFactory.NewWindowInputSource(gameContext);
+                _gameWindowRenderer = new GameWindowRenderer(registry, gameContext);
+                _schedulerSystem = registry.GetService<SchedulerSystem>();
+
+                var manager = _gameWindowRenderer.WindowManager;
+                {
+                    manager.PreferredBackBufferWidth = 640;
+                    manager.PreferredBackBufferHeight = 480;
+                    manager.PreferredBackBufferFormat = PixelFormat.R16G16B16A16_Float;
+                    manager.PreferredDepthStencilFormat = PixelFormat.D24_UNorm_S8_UInt;
+                    manager.ShaderProfile = GraphicsProfile.Level_11_0;
+                    manager.PreferredGraphicsProfile = [GraphicsProfile.Level_11_0];
+
+                    using (var dev = nodeContext.AppHost.Services.GetDeviceHandle())
+                    {
+                        manager.PreferredColorSpace = dev.Resource.ColorSpace;
+                    }
+
+                    manager.PreferredMultisampleCount = MultisampleCount.None;
+                }
+
+                _gameWindowRenderer.Initialize();
+
+                if (_gameWindowRenderer is IContentable contentable)
+                {
+                    contentable.LoadContent();
+                }
+
+                var window = _gameWindowRenderer.Window;
+                {
+                    window.Position = new Int2(100, 100);
+                    window.FullscreenIsBorderlessWindow = true;
+                    window.PreferredFullscreenSize = new Int2(-1, -1); // adapt desktop Size
+                    window.AllowUserResizing = true;
+                    window.IsMouseVisible = true;
+                    window.BringToFront();
+                }
+
+                _windowRenderer = new WindowRenderer(_gameWindowRenderer);
+            }
+
+            vp.PlatformUserData = (IntPtr)_gcHandle;
+        }
+
+        public void Update(IGraphicsRendererBase IRenderer)
+        {
+            _windowRenderer.Input = IRenderer;
+            _schedulerSystem.Schedule(_windowRenderer);
+        }
+
+        public void Dispose()
+        {
+            _gameWindowRenderer.Close();
+            _inputSource.Dispose();
+            _gcHandle.Free();
+        }
+
+
+        class WindowRenderer : IGraphicsRendererBase
+        {
+            private readonly GameWindowRenderer _gameWindowRenderer;
+            private readonly WithRenderTargetAndViewPort _withRenderTargetAndViewPort;
+
+            public IGraphicsRendererBase? Input {  private get; set; }
+
+            public WindowRenderer(GameWindowRenderer gameWindowRenderer)
+            {
+                _gameWindowRenderer = gameWindowRenderer;
+                _withRenderTargetAndViewPort = new WithRenderTargetAndViewPort();
+            }
+
+            public void Draw(RenderDrawContext context)
+            {
+                if (Input != null)
+                {
+                    var bdraw = _gameWindowRenderer.BeginDraw();
+                    if (bdraw)
+                    {
+                        _withRenderTargetAndViewPort.RenderTarget = _gameWindowRenderer.Presenter.BackBuffer;
+                        _withRenderTargetAndViewPort.DepthBuffer = _gameWindowRenderer.Presenter.DepthStencilBuffer;
+                        _withRenderTargetAndViewPort.Input = Input;
+                        _withRenderTargetAndViewPort.Draw(context);
+                    }
+                    _gameWindowRenderer.EndDraw();
+                }
+            }
+        }
+    }
+
+
+
+
+   
+}
---
+++ VL.ImGui.Stride/src/ImGuiWindows.cs
@@ -0,0 +1,221 @@
+﻿using ImGuiNET;
+
+using System.Runtime.InteropServices;
+using Stride.Core.Mathematics;
+using VL.Core;
+using Stride.Engine;
+using VL.Lib.Basics.Resources;
+using VL.Stride.Engine;
+using VL.Stride.Games;
+using VL.Stride;
+using Stride.Games;
+using Stride.Rendering;
+
+
+namespace VL.ImGui.Stride
+{
+    using ImGui = ImGuiNET.ImGui;
+    using Vector2 = System.Numerics.Vector2;
+
+    public partial class ImGuiWindows : IDisposable
+    {
+        //VL 
+        private readonly NodeContext nodeContext;
+        private readonly ImGuiWindow mainViewportWindow;
+
+        private readonly Platform_CreateWindow _createWindow;
+        private readonly Platform_DestroyWindow _destroyWindow;
+        private readonly Platform_GetWindowPos _getWindowPos;
+        private readonly Platform_ShowWindow _showWindow;
+        private readonly Platform_SetWindowPos _setWindowPos;
+        private readonly Platform_SetWindowSize _setWindowSize;
+        private readonly Platform_GetWindowSize _getWindowSize;
+        private readonly Platform_SetWindowFocus _setWindowFocus;
+        private readonly Platform_GetWindowFocus _getWindowFocus;
+        private readonly Platform_GetWindowMinimized _getWindowMinimized;
+        private readonly Platform_SetWindowTitle _setWindowTitle;
+
+        public unsafe ImGuiWindows(NodeContext nodeContext)
+        {
+            this.nodeContext = nodeContext;
+
+            
+
+            IntPtr context = ImGui.CreateContext();
+            ImGui.SetCurrentContext(context);
+            ImGuiIOPtr io = ImGui.GetIO();
+
+            io.ConfigFlags |= ImGuiConfigFlags.DockingEnable;
+            io.ConfigFlags |= ImGuiConfigFlags.ViewportsEnable;
+
+            ImGuiPlatformIOPtr platformIO = ImGui.GetPlatformIO();
+            ImGuiViewportPtr mainViewport = platformIO.Viewports[0];
+
+            mainViewportWindow = new ImGuiWindow(this.nodeContext, mainViewport);
+            mainViewport.PlatformHandle = mainViewportWindow.Handle;
+            
+
+            _createWindow = CreateWindow;
+            _destroyWindow = DestroyWindow;
+            _getWindowPos = GetWindowPos;
+            _showWindow = ShowWindow;
+            _setWindowPos = SetWindowPos;
+            _setWindowSize = SetWindowSize;
+            _getWindowSize = GetWindowSize;
+            _setWindowFocus = SetWindowFocus;
+            _getWindowFocus = GetWindowFocus;
+            _getWindowMinimized = GetWindowMinimized;
+            _setWindowTitle = SetWindowTitle;
+
+            platformIO.Platform_CreateWindow = Marshal.GetFunctionPointerForDelegate(_createWindow);
+            platformIO.Platform_DestroyWindow = Marshal.GetFunctionPointerForDelegate(_destroyWindow);
+            platformIO.Platform_ShowWindow = Marshal.GetFunctionPointerForDelegate(_showWindow);
+            platformIO.Platform_SetWindowPos = Marshal.GetFunctionPointerForDelegate(_setWindowPos);
+            platformIO.Platform_SetWindowSize = Marshal.GetFunctionPointerForDelegate(_setWindowSize);
+            platformIO.Platform_SetWindowFocus = Marshal.GetFunctionPointerForDelegate(_setWindowFocus);
+            platformIO.Platform_GetWindowFocus = Marshal.GetFunctionPointerForDelegate(_getWindowFocus);
+            platformIO.Platform_GetWindowMinimized = Marshal.GetFunctionPointerForDelegate(_getWindowMinimized);
+            platformIO.Platform_SetWindowTitle = Marshal.GetFunctionPointerForDelegate(_setWindowTitle);
+
+            ImGuiNative.ImGuiPlatformIO_Set_Platform_GetWindowPos(platformIO.NativePtr, Marshal.GetFunctionPointerForDelegate(_getWindowPos));
+            ImGuiNative.ImGuiPlatformIO_Set_Platform_GetWindowSize(platformIO.NativePtr, Marshal.GetFunctionPointerForDelegate(_getWindowSize));
+        }
+
+        public void Update(IGraphicsRendererBase IRenderer)
+        {
+            mainViewportWindow.Update(IRenderer);
+        }
+
+
+        #region Platformfunctions
+        
+        private void CreateWindow(ImGuiViewportPtr vp)
+        {
+            ImGuiWindow window = new ImGuiWindow(nodeContext, vp);
+        }
+
+        private void DestroyWindow(ImGuiViewportPtr vp)
+        {
+            if (vp.PlatformUserData != IntPtr.Zero)
+            {
+                var target = GCHandle.FromIntPtr(vp.PlatformUserData).Target;
+
+                if (target != null)
+                {
+                    ImGuiWindow window = (ImGuiWindow)target;
+                    window.Dispose();
+                }
+
+                vp.PlatformUserData = IntPtr.Zero;
+            }
+        }
+
+        private void ShowWindow(ImGuiViewportPtr vp)
+        {
+            var target = GCHandle.FromIntPtr(vp.PlatformUserData).Target;
+            if (target != null)
+            {
+                ImGuiWindow window = (ImGuiWindow)target;
+                window.Activate();
+            }
+        }
+
+        private unsafe void GetWindowPos(ImGuiViewportPtr vp, Vector2* outPos)
+        {
+            var target = GCHandle.FromIntPtr(vp.PlatformUserData).Target;
+            if (target != null)
+            {
+                ImGuiWindow window = (ImGuiWindow)target;
+                *outPos = new Vector2(window.Position.X, window.Position.Y);
+            }
+        }
+
+        private void SetWindowPos(ImGuiViewportPtr vp, Vector2 pos)
+        {
+            var target = GCHandle.FromIntPtr(vp.PlatformUserData).Target;
+            if (target != null)
+            {
+                ImGuiWindow window = (ImGuiWindow)target;
+                window.Position = new Int2((int)pos.X, (int)pos.Y);
+            }
+        }
+        private unsafe void GetWindowSize(ImGuiViewportPtr vp, Vector2* outSize)
+        {
+            var target = GCHandle.FromIntPtr(vp.PlatformUserData).Target;
+            if (target != null)
+            {
+                ImGuiWindow window = (ImGuiWindow)target;
+                *outSize = new Vector2(window.Size.X, window.Size.Y);
+            }
+        }
+
+        private void SetWindowSize(ImGuiViewportPtr vp, Vector2 size)
+        {
+            var target = GCHandle.FromIntPtr(vp.PlatformUserData).Target;
+            if (target != null)
+            {
+                ImGuiWindow window = (ImGuiWindow)target;
+                window.Size = new Int2((int)size.X, (int)size.Y);
+            }
+        }
+
+        private void SetWindowFocus(ImGuiViewportPtr vp)
+        {
+            var target = GCHandle.FromIntPtr(vp.PlatformUserData).Target;
+            if (target != null)
+            {
+                ImGuiWindow window = (ImGuiWindow)target;
+                window.Activate();
+            }
+        }
+
+        private byte GetWindowFocus(ImGuiViewportPtr vp)
+        {
+            bool isActivated = false;
+            var target = GCHandle.FromIntPtr(vp.PlatformUserData).Target;
+            if (target != null)
+            {
+                ImGuiWindow window = (ImGuiWindow)target;
+                isActivated = window.IsActivated;
+            }
+
+            return isActivated ? (byte)1 : (byte)0;
+        }
+
+        private byte GetWindowMinimized(ImGuiViewportPtr vp)
+        {
+            bool isMinimized = false;
+            var target = GCHandle.FromIntPtr(vp.PlatformUserData).Target;
+            if (target != null)
+            {
+                ImGuiWindow window = (ImGuiWindow)target;
+                isMinimized = window.IsMinimized;
+            }
+
+            return isMinimized ? (byte)1 : (byte)0;
+        }
+
+        private unsafe void SetWindowTitle(ImGuiViewportPtr vp, IntPtr title)
+        {
+            var target = GCHandle.FromIntPtr(vp.PlatformUserData).Target;
+            if (target != null)
+            {
+                ImGuiWindow window = (ImGuiWindow)target;
+
+                byte* titlePtr = (byte*)title;
+                int count = 0;
+                while (titlePtr[count] != 0)
+                {
+                    count += 1;
+                }
+                window.Title = System.Text.Encoding.ASCII.GetString(titlePtr, count);
+            }
+        }
+        #endregion Platformfunctions
+
+        public void Dispose()
+        {
+            mainViewportWindow.Dispose();
+        }
+    }
+}
