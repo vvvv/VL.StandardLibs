8aa1140646e9d4c8848a59c656766799c456c403
antongit <antonworx@gmail.com>
2024-05-15 19:15:01 +0200

    ImGui: added Keyboard Queries. Adds to #39.

---
+++ VL.ImGui/src/Widgets/Queries/Keyboard/IsKeyPressed.cs
@@ -0,0 +1,23 @@
+﻿using VL.Lib.IO;
+
+namespace VL.ImGui.Widgets
+{
+    /// <summary>
+    /// Is key pressed?
+    /// </summary>
+    [GenerateNode(Category = "ImGui.Queries")]
+    internal partial class IsKeyPressed : Query
+    {
+
+        public Keys Key { private get; set; }
+
+        public bool Repeat { private get; set; }
+
+        public bool Value { get; private set; }
+
+        internal override void UpdateCore(Context context)
+        {
+            Value = ImGuiNET.ImGui.IsKeyPressed(Key.ToImGuiKey(), Repeat);
+        }
+    }
+}
