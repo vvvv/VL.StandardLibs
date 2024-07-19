2b275a0093df063bef07e2badf193bbf6aa85e20
Elias Holzer <elias@vvvv.org>
2024-07-19 21:31:50 +0200

    Couple of fixes to Synchronizer regions:
    - All regions share the same code path
    - They now use the same helper classes as the normal `ForEach` to build the output spread -> no longer allocate new spreads each frame if nothing changes
    - They're all swappable now leading to less outdated issues
    - Fixes crash on VLObjectInput version when changing the signature of the incoming type (swappable implementation didn't work)
    - They no longer allocate if nothing changes (which usually is the normal case)

--- VL.CoreLib/src/Control/Synchronize.cs
+++ VL.CoreLib/src/Control/Synchronize.cs
@@ -1,24 +1,102 @@
-﻿using System;
+﻿#nullable enable
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Reflection;
 using VL.Core;
 using VL.Core.CompilerServices;
+using VL.Core.Utils;
 using VL.Lib.Collections;
 
 namespace VL.Lib.Control
 {
-    public class SynchronizerInputIsKey<TState, TInput, TOutput> : IDisposable
+    internal sealed class Synchronizer<TKey, TState, TInput, TOutput> : IDisposable, ISwappableGenericType
+        where TKey : notnull
     {
-        readonly ImmutableDictionary<TInput, TState> States;
+        private readonly ImmutableDictionary<TKey, TState> states;
+        private readonly Spread<TOutput> outputs;
 
-        public SynchronizerInputIsKey(ImmutableDictionary<TInput, TState> states)
+        private Synchronizer(ImmutableDictionary<TKey, TState> states, Spread<TOutput> outputs)
         {
-            States = states ?? ImmutableDictionary<TInput, TState>.Empty;
+            this.states = states;
+            this.outputs = outputs;
+        }
+
+        public Synchronizer()
+            : this(ImmutableDictionary<TKey, TState>.Empty, Spread<TOutput>.Empty)
+        {
+        }
+
+        public Synchronizer<TKey, TState, TInput, TOutput> Update(
+            IEnumerable<TInput> input,
+            Func<TInput, TKey> keySelector,
+            Func<TInput, TState> create,
+            Func<TState, TInput, Tuple<TState, TOutput>> updator,
+            out Spread<TOutput> outputs)
+        {
+            var oldStates = this.states.ToBuilder();
+            var newStatesBuilder = CollectionBuilders.GetBuilder(this.states, 0);
+
+            var outputsBuilder = CollectionBuilders.GetBuilder(this.outputs, 0);
+            foreach (var i in input)
+            {
+                var key = keySelector(i);
+                oldStates.Remove(key);
+            }
+            foreach (var dying in oldStates.Values)
+                (dying as IDisposable)?.Dispose();
+            foreach (var i in input)
+            {
+                var key = keySelector(i);
+                if (!this.states.TryGetValue(key, out var state))
+                    state = create(i);
+                TOutput output;
+                (state, output) = updator(state, i);
+                newStatesBuilder.Add(key, state);
+                outputsBuilder.Add(output);
+            }
+
+            outputs = outputsBuilder.Commit();
+            var states = newStatesBuilder.Commit();
+            if (outputs != this.outputs || states != this.states)
+                return new Synchronizer<TKey, TState, TInput, TOutput>(states, outputs);
+            return this;
+        }
+
+        public void Dispose()
+        {
+            foreach (var s in states.Values)
+                if (s is IDisposable d)
+                    d.Dispose();
+        }
+
+        object? ISwappableGenericType.Swap(Type newType, Swapper swapObject)
+        {
+            var args = newType.GetGenericArguments();
+            var newTKey = args[0];
+            var newTState = args[1];
+            var newTOutput = args[3];
+            var newdictType = typeof(ImmutableDictionary<,>).MakeGenericType(newTKey, newTState);
+            var newdict = swapObject(states, newdictType);
+            var newSpreadType = typeof(Spread<>).MakeGenericType(newTOutput);
+            var newspread = swapObject(outputs, newSpreadType);
+            return Activator.CreateInstance(newType, binder: null, bindingAttr: BindingFlags.Instance | BindingFlags.NonPublic, args: [newdict, newspread], culture: null);
+        }
+    }
+
+    public sealed class SynchronizerInputIsKey<TState, TInput, TOutput> : IDisposable, ISwappableGenericType
+        where TInput : notnull
+    {
+        private readonly Synchronizer<TInput, TState, TInput, TOutput> impl;
+
+        private SynchronizerInputIsKey(Synchronizer<TInput, TState, TInput, TOutput> impl)
+        {
+            this.impl = impl;
         }
 
         public SynchronizerInputIsKey()
-            : this(null)
+            : this(new())
         {
         }
 
@@ -28,49 +106,34 @@ namespace VL.Lib.Control
             Func<TState, TInput, Tuple<TState, TOutput>> updator,
             out Spread<TOutput> outputs)
         {
-            var oldStates = States.ToBuilder();
-            var newStates = ImmutableDictionary<TInput, TState>.Empty.ToBuilder();
-            var outputsBuilder = SpreadBuilder<TOutput>.Empty;
-            foreach (var key in input)
-                oldStates.Remove(key);
-            foreach (var dying in oldStates.Values)
-                (dying as IDisposable)?.Dispose();
-            foreach (var i in input)
-            {
-                var key = i;
-                if (!States.TryGetValue(key, out TState state))
-                    state = create(i);
-                TOutput output;
-                (state, output) = updator(state, i);
-                newStates[key] = state;
-                outputsBuilder.Add(output);
-            }
-
-            outputs = outputsBuilder.ToSpread();
-            return new SynchronizerInputIsKey<TState, TInput, TOutput>(newStates.ToImmutable());
+            var impl = this.impl.Update(input, i => i, create, updator, out outputs);
+            if (impl != this.impl)
+                return new(impl);
+            return this;
         }
 
-        public void Dispose()
+        public void Dispose() => impl.Dispose();
+
+        object? ISwappableGenericType.Swap(Type newType, Swapper swapObject)
         {
-            foreach (var s in States.Values)
-                if (s is IDisposable d)
-                    d.Dispose();
+            var args = newType.GetGenericArguments();
+            var newTState = args[0];
+            var newTInput = args[1];
+            var newTOutput = args[2];
+            var newImplType = typeof(Synchronizer<,,,>).MakeGenericType(newTInput, newTState, newTInput, newTOutput);
+            var newimpl = swapObject(this.impl, newImplType);
+            return Activator.CreateInstance(newType, binder: null, bindingAttr: BindingFlags.Instance | BindingFlags.NonPublic, args: [newimpl], culture: null);
         }
     }
 
-    public class Synchronizer<TState, TInput, TOutput> : IDisposable
+    public sealed class Synchronizer<TState, TInput, TOutput> : IDisposable, ISwappableGenericType
+        where TInput : notnull
     {
-        readonly ImmutableDictionary<object, TState> States;
+        private readonly Synchronizer<object, TState, TInput, TOutput> impl;
 
-        public Synchronizer(ImmutableDictionary<object, TState> states)
-        {
-            States = states ?? ImmutableDictionary<object, TState>.Empty;
-        }
+        public Synchronizer() : this(new()) { }
 
-        public Synchronizer()
-            : this(null)
-        {
-        }
+        private Synchronizer(Synchronizer<object, TState, TInput, TOutput> impl) => this.impl = impl;
 
         public Synchronizer<TState, TInput, TOutput> Update(
             IEnumerable<TInput> input, 
@@ -79,52 +142,31 @@ namespace VL.Lib.Control
             Func<TState, TInput, Tuple<TState, TOutput>> updator,
             out Spread<TOutput> outputs)
         {
-            var oldStates = States.ToBuilder();
-            var newStates = ImmutableDictionary<object, TState>.Empty.ToBuilder();
-            var outputsBuilder = SpreadBuilder<TOutput>.Empty;
-            foreach (var i in input)
-            {
-                var key = keySelector(i);
-                oldStates.Remove(key);
-            }
-            foreach (var dying in oldStates.Values)
-                (dying as IDisposable)?.Dispose();
-            foreach (var i in input)
-            {
-                var key = keySelector(i);
-                if (!States.TryGetValue(key, out TState state))
-                    state = create(i);
-                TOutput output;
-                (state, output) = updator(state, i);
-                newStates[key] = state;
-                outputsBuilder.Add(output);
-            }
-            outputs = outputsBuilder.ToSpread();
-            return new Synchronizer<TState, TInput, TOutput>(newStates.ToImmutable());
+            var impl = this.impl.Update(input, keySelector, create, updator, out outputs);
+            if (impl != this.impl)
+                return new(impl);
+            return this;
         }
 
-        public void Dispose()
+        public void Dispose() => impl.Dispose();
+
+        object? ISwappableGenericType.Swap(Type newType, Swapper swapObject)
         {
-            foreach (var s in States.Values)
-                if (s is IDisposable d)
-                    d.Dispose();
+            var args = newType.GetGenericArguments();
+            var newImplType = typeof(Synchronizer<,,,>).MakeGenericType(typeof(object), args[0], args[1], args[2]);
+            var newimpl = swapObject(this.impl, newImplType);
+            return Activator.CreateInstance(newType, binder: null, bindingAttr: BindingFlags.Instance | BindingFlags.NonPublic, args: [newimpl], culture: null);
         }
     }
 
-    public class SynchronizerVLObjectInput<TState, TInput, TOutput> : IDisposable, ISwappableGenericType
+    public sealed class SynchronizerVLObjectInput<TState, TInput, TOutput> : IDisposable, ISwappableGenericType
         where TInput: IVLObject
     {
-        readonly ImmutableDictionary<object, TState> States;
+        private readonly Synchronizer<uint, TState, TInput, TOutput> impl;
 
-        public SynchronizerVLObjectInput(ImmutableDictionary<object, TState> states)
-        {
-            States = states ?? ImmutableDictionary<object, TState>.Empty;
-        }
+        private SynchronizerVLObjectInput(Synchronizer<uint, TState, TInput, TOutput> impl) => this.impl = impl;
 
-        public SynchronizerVLObjectInput()
-            : this(null)
-        {
-        }
+        public SynchronizerVLObjectInput() : this(new()) { }
 
         public SynchronizerVLObjectInput<TState, TInput, TOutput> Update(
             IEnumerable<TInput> input,
@@ -132,44 +174,20 @@ namespace VL.Lib.Control
             Func<TState, TInput, Tuple<TState, TOutput>> updator,
             out Spread<TOutput> outputs)
         {
-            var oldStates = States.ToBuilder();
-            var newStates = ImmutableDictionary<object, TState>.Empty.ToBuilder();
-            var outputsBuilder = SpreadBuilder<TOutput>.Empty;
-            foreach (var i in input)
-            {
-                var key = i.Identity;
-                oldStates.Remove(key);
-            }
-            foreach (var dying in oldStates.Values)
-                (dying as IDisposable)?.Dispose();
-            foreach (var i in input)
-            {
-                var key = i.Identity;
-                if (!States.TryGetValue(key, out TState state))
-                    state = create(i);
-                TOutput output;
-                (state, output) = updator(state, i);
-                newStates[key] = state;
-                outputsBuilder.Add(output);
-            }
-
-            outputs = outputsBuilder.ToSpread();
-            return new SynchronizerVLObjectInput<TState, TInput, TOutput>(newStates.ToImmutable());
+            var impl = this.impl.Update(input, i => i.Identity, create, updator, out outputs);
+            if (impl != this.impl)
+                return new(impl);
+            return this;
         }
 
-        public void Dispose()
-        {
-            foreach (var s in States.Values)
-                if (s is IDisposable d)
-                    d.Dispose();
-        }
+        public void Dispose() => impl.Dispose();
 
-        object ISwappableGenericType.Swap(Type newType, Swapper swapObject)
+        object? ISwappableGenericType.Swap(Type newType, Swapper swapObject)
         {
-            var newTState = newType.GetGenericArguments().Single(_ => _.Name == nameof(TState));
-            var newdictType = typeof(ImmutableDictionary<,>).MakeGenericType(typeof(object), newTState);
-            var newdict = swapObject(States, newdictType);
-            return Activator.CreateInstance(newType, args: newdict);
+            var args = newType.GetGenericArguments();
+            var newImplType = typeof(Synchronizer<,,,>).MakeGenericType(typeof(uint), args[0], args[1], args[2]);
+            var newimpl = swapObject(this.impl, newImplType);
+            return Activator.CreateInstance(newType, binder: null, bindingAttr: BindingFlags.Instance | BindingFlags.NonPublic, args: [newimpl], culture: null);
         }
     }
 }
