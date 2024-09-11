2f288fe98acc7c4576943a83c33004e00aca6443
bj-rn <schwarzer@posteo.net>
2024-09-11 11:16:47 +0200

    now taking into account that BC6H is a 16 bit format

--- VL.Stride.Runtime/src/Rendering/Effects/EffectShaderNodes.TextureFX.cs
+++ VL.Stride.Runtime/src/Rendering/Effects/EffectShaderNodes.TextureFX.cs
@@ -373,12 +373,16 @@ namespace VL.Stride.Rendering
                                     var desc = (size: defaultSize, format: defaultFormat, renderFormat: defaultRenderFormat);
                                     if (inputTexture != null)
                                     {
+
                                         // Base it on the input texture if it is not block compressed (BC1 - B7)
-                                        var viewFormat = (inputTexture.ViewFormat.IsCompressed(), inputTexture.ViewFormat.IsSRgb()) switch
+                                        var viewFormat = inputTexture.ViewFormat;
+
+                                        viewFormat = (viewFormat.IsCompressed(), viewFormat.IsSRgb(), viewFormat.IsHDR()) switch
                                         {
-                                            (false, _) => inputTexture.ViewFormat,
-                                            (true, false) => PixelFormat.R8G8B8A8_UNorm,
-                                            (true, true) => PixelFormat.R8G8B8A8_UNorm_SRgb
+                                            (false, _, _) => viewFormat,
+                                            (true, false, false) => PixelFormat.R8G8B8A8_UNorm,
+                                            (true, true, false) => PixelFormat.R8G8B8A8_UNorm_SRgb,
+                                            (true, _, true) => PixelFormat.R16G16B16A16_Float
                                         };
                                       
                                         // Figure out render format
