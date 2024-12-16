a461a82c1147fcbfb627266dc2b696662b228713
Johannes Schmidt <schmidt@kopffarben.de>
2024-12-16 18:59:53 +0100

    Add UniqueId to RenderWidget for unique naming
    Introduce UniqueId field to RenderWidget for unique naming.
    Add constructors to initialize UniqueId. Remove nodeContext property.
    Update ImGui.BeginChild to use UniqueId for unique names.

--- VL.ImGui.Stride/src/RenderWidget.cs
+++ VL.ImGui.Stride/src/RenderWidget.cs
@@ -12,10 +12,26 @@ namespace VL.ImGui.Widgets
     [GenerateNode(Category = "ImGui.Widgets.Internal", IsStylable = false)]
     public sealed partial class RenderWidget : Widget, IDisposable
     {
+        // UniqueId is used for Uniqe Name, so Menu Layout can be saved
+        private readonly UniqueId UniqueId;
+
+        // if you use a Constructor with NodeContext
+        // and that is the only option the CodeGen gives you
+        // than you need also a empty Constructor 
+        public RenderWidget(NodeContext nodeContext)
+        {
+            // ?? why Pop().Peek(), Peek() always the same
+            UniqueId = nodeContext.Path.Stack.Pop().Peek();
+        }
+
+        // empty Constructor
+        public RenderWidget()
+        {
+            UniqueId = new UniqueId();
+        }
+
         private RenderLayerWithViewPort? renderLayer;
         private IContextWithRenderer? strideContext;
-
-        public NodeContext? nodeContext { private get; set; }
 
         public IGraphicsRendererBase? Layer { private get; set ; }
 
@@ -40,7 +56,7 @@ namespace VL.ImGui.Widgets
                     return;
                 }
                
-                if (ImGui.BeginChild("##RenderWidget__" + nodeContext?.Path.Stack.Peek().ToString() , Size.FromHectoToImGui(), ImGuiChildFlags.None, ImGuiWindowFlags.ChildWindow))
+                if (ImGui.BeginChild("##RenderWidget__" + UniqueId.ToString() , Size.FromHectoToImGui(), ImGuiChildFlags.None, ImGuiWindowFlags.ChildWindow))
                 {
                     if (renderLayer.Layer != Layer)
                         renderLayer.Layer = Layer;
