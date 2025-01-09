56b86132b4a5f79fd6e3903b8ddac8c0fd881617
Elias Holzer <elias@vvvv.org>
2025-01-09 20:39:03 +0100

    WIP

--- VL.Core/src/MonadicValues.cs
+++ VL.Core/src/MonadicValues.cs
@@ -114,4 +114,87 @@ namespace VL.Core
         TMonad SetValue(TMonad monad, TValue? value);
         bool HasCustomDefault => false;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /// <summary>
+    /// Marks a type as monadic - it can wrap any basic type. The value wrapping is done through an intermediate as specified by the given factory.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface, AllowMultiple = false, Inherited = false)]
+    public class MonadicAttribute : Attribute
+    {
+        public Type Factory { get; }
+
+        /// <summary>
+        /// An optional type filter. The type must have a default constructor and implement <see cref="IMonadicTypeFilter"/>.
+        /// </summary>
+        public Type? TypeFilter { get; }
+
+        public MonadicAttribute(Type factory)
+        {
+            Factory = factory;
+        }
+
+        public MonadicAttribute(Type factory, Type typeFilter)
+        {
+            Factory = factory;
+            TypeFilter = typeFilter;
+        }
+    }
+
+    /// <summary>
+    /// Implementations must have a default constructor as well as a static readonly field called "Default".
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value</typeparam>
+    /// <typeparam name="TMonad">The type of the monadic value</typeparam>
+    public interface IMonadicFactory<TValue, TMonad>
+    {
+        /// <summary>
+        /// Creates a monad builder.
+        /// </summary>
+        /// <param name="isConstant">Whether or not the value is constant.</param>
+        /// <returns>The monad builder</returns>
+        IMonadBuilder<TValue, TMonad> GetMonadBuilder(bool isConstant);
+
+        /// <summary>
+        /// Creates a monad builder.
+        /// </summary>
+        /// <param name="isConstant">Whether or not the value is constant.</param>
+        /// <param name="nodeContext">The node context - its path will have the id of the sink appended to it.</param>
+        /// <returns>The monad builder</returns>
+        IMonadBuilder<TValue, TMonad> GetMonadBuilder(bool isConstant, NodeContext nodeContext) => GetMonadBuilder(isConstant);
+    }
+
+    /// <summary>
+    /// Builds the monadic value <typeparamref name="TMonad"/> out of <typeparamref name="TValue"/>.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value</typeparam>
+    /// <typeparam name="TMonad">The type of the monadic value</typeparam>
+    public interface IMonadBuilder<TValue, TMonad>
+    {
+        TMonad Return(TValue value);
+
+        /// <summary>
+        /// Called when the system has no value yet. This is usually true for unconnected input pins.
+        /// </summary>
+        TMonad? Default() => default;
+    }
 }
