7512a8eddafd6ba34d4ae93e3ed1f33bf5d52068
Elias Holzer <elias@vvvv.org>
2025-01-16 14:49:35 +0100

    [Video] Adds IPixel implementations for R8, R16, R32F and R32G32F

---
+++ VL.Core/src/Types/Video/Pixels/R16Pixel.cs
@@ -0,0 +1,11 @@
+﻿#nullable enable
+using VL.Lib.Basics.Imaging;
+
+namespace VL.Lib.Basics.Video
+{
+    public record struct R16Pixel(byte R) : IPixel
+    {
+        public PixelFormat PixelFormat => PixelFormat.R16;
+    }
+}
+#nullable restore
---
+++ VL.Core/src/Types/Video/Pixels/R8Pixel.cs
@@ -0,0 +1,11 @@
+﻿#nullable enable
+using VL.Lib.Basics.Imaging;
+
+namespace VL.Lib.Basics.Video
+{
+    public record struct R8Pixel(byte R) : IPixel
+    {
+        public PixelFormat PixelFormat => PixelFormat.R8;
+    }
+}
+#nullable restore
