d1463c9df24f0c8f364bc3c7c4f9c0393d27052b
Alexander Shaliapin <a.shaliapin@gmail.com>
2025-01-17 15:30:54 +0100

    fixes R16Pixel video type

--- VL.Core/src/Types/Video/Pixels/R16Pixel.cs
+++ VL.Core/src/Types/Video/Pixels/R16Pixel.cs
@@ -3,7 +3,7 @@ using VL.Lib.Basics.Imaging;
 
 namespace VL.Lib.Basics.Video
 {
-    public record struct R16Pixel(byte R) : IPixel
+    public record struct R16Pixel(ushort R) : IPixel
     {
         public PixelFormat PixelFormat => PixelFormat.R16;
     }
