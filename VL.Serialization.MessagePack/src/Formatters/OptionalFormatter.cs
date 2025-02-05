10cc9b283abe8aa5996be2d2bce18561b3562133
Elias Holzer <elias@vvvv.org>
2025-02-05 20:30:22 +0100

    [MessagePack] Adds serializer for `Optional<T>`

---
+++ VL.Serialization.MessagePack/src/Formatters/OptionalFormatter.cs
@@ -0,0 +1,45 @@
+﻿using MessagePack.Formatters;
+using MessagePack;
+using VL.Core;
+
+namespace VL.Serialization.MessagePack.Formatters
+{
+    sealed class OptionalFormatter<T> : IMessagePackFormatter<Optional<T>>
+    {
+        IMessagePackFormatter<T>? formatter = null;
+
+        public void Serialize(ref MessagePackWriter writer, Optional<T> value, MessagePackSerializerOptions options)
+        {
+            if (value == default)
+            {
+                writer.WriteNil();
+            }
+            else
+            {
+                if (formatter == null)
+                {
+                    formatter = options.Resolver.GetFormatterWithVerify<T>();
+                }
+
+                formatter.Serialize(ref writer, value.Value, options);
+            }
+        }
+
+        public Optional<T> Deserialize(ref MessagePackReader reader, MessagePackSerializerOptions options)
+        {
+            if (reader.TryReadNil())
+            {
+                return default;
+            }
+            else
+            {
+                if (formatter == null)
+                {
+                    formatter = options.Resolver.GetFormatterWithVerify<T>();
+                }
+
+                return formatter.Deserialize(ref reader, options);
+            }
+        }
+    }
+}
