vvvv/VL.StandardLibs

commit 57aade5033fc14cfe8433fe5bda32ec4b80a0266

work on EditMode, InputSource management, Peg

VL.Stride.Runtime/src/Input/InputSourceComponent.cs
@@ -32,19 +32,15 @@ namespace VL.Stride.Input
         {
             var inputSource = context.GetWindowInputSource();
 
-            if (inputSource != null)
+            foreach (var entityKeyPair in ComponentDatas)
             {
-                foreach (var entityKeyPair in ComponentDatas)
-                {
-                    var component = entityKeyPair.Key;
-                    //var associatedData = entityKeyPair.Value;
+                var component = entityKeyPair.Key;
+                //var associatedData = entityKeyPair.Value;
 
-                    if (component.Enabled)
-                        component.InputSource = inputSource;
-                    else
-                        component.InputSource = null;
-                }
-
+                if (component.Enabled)
+                    component.InputSource = inputSource;
+                else
+                    component.InputSource = null;
             }
             base.Draw(context);
         }

VL.Stride.Runtime/src/Rendering/Renderers/WithWindowInputSource.cs
@@ -11,16 +11,8 @@ namespace VL.Stride.Rendering
 
         protected override void DrawInternal(RenderDrawContext context)
         {
-
             var inputSource = InputSource;
-            if (inputSource != null)
-            {
-                using (context.RenderContext.PushTagAndRestore(InputExtensions.WindowInputSource, inputSource))
-                {
-                    DrawInput(context);
-                }
-            }
-            else
+            using (context.RenderContext.PushTagAndRestore(InputExtensions.WindowInputSource, inputSource))
             {
                 DrawInput(context);
             }

VL.Stride.Runtime/src/Engine/EntityUtils.cs
@@ -1,4 +1,5 @@
-﻿using Stride.Core;
+﻿using SharpFont.Cache;
+using Stride.Core;
 using Stride.Engine;
 using System;
 using System.Collections.Generic;
@@ -93,5 +94,36 @@ namespace VL.Stride.Engine
         {
             entity.Enable<T>(enabled, applyOnChildren);
         }
+
+
+
+
+
+
+        public interface IPatchedEntity
+        {
+            Entity Entity { get; }
+        }
+
+
+        public interface IEditMode
+        {
+            void AddPatchedEntity(IPatchedEntity patchedEntity);
+            void RemovePatchedEntity(IPatchedEntity patchedEntity);
+        }
+
+
+
+        private static readonly PropertyKey<IEditMode> editModeKey = new PropertyKey<IEditMode>("EditMode", typeof(EntityUtils));
+
+        public static Scene WithEditMode(this Scene scene, IEditMode editmode, NodeContext nodeContext)
+        {
+            scene.Tags.Set(editModeKey, editmode);
+            return scene;
+        }
+
+        public static IEditMode GetEditMode(this Scene scene)
+            => scene.Tags.Get(editModeKey);
+
     }
 }

VL.Stride.Runtime/src/Input/CameraInputSourceComponent.cs
@@ -45,7 +45,7 @@ namespace VL.Stride.Input
 
         protected override void DrawCore(RenderContext context, RenderDrawContext drawContext)
         {
-            var inputSource = InputSource ?? context.GetWindowInputSource();
+            var inputSource = Enabled ? InputSource ?? context.GetWindowInputSource() : null;
 
             if (CameraInputSourceComponent != null)
             {

commit ff90721c9f52f278f09f9993e135ea63bc04194c

[Stride] Fixes Mouse, Keyboard, Touch no longer working correctly in multi-window mode
Regression was introduced with 57aade5033fc14cfe8433fe5bda32ec4b80a0266

VL.Stride.Runtime/src/Input/EmptyInputSource.cs
@@ -1,0 +1,27 @@
+﻿using Stride.Core.Collections;
+using Stride.Input;
+using System;
+
+namespace VL.Stride.Input
+{
+    sealed class EmptyInputSource : IInputSource
+    {
+        public static readonly EmptyInputSource Instance = new();
+
+        private EmptyInputSource() { }
+
+        TrackingDictionary<Guid, IInputDevice> IInputSource.Devices { get; } = new();
+
+        void IDisposable.Dispose() { }
+
+        void IInputSource.Initialize(InputManager inputManager) { }
+
+        void IInputSource.Pause() { }
+
+        void IInputSource.Resume() { }
+
+        void IInputSource.Scan() { }
+
+        void IInputSource.Update() { }
+    }
+}

VL.Stride.Runtime/src/Input/InputSourceComponent.cs
@@ -32,16 +32,20 @@ namespace VL.Stride.Input
         {
             var inputSource = context.GetWindowInputSource();
 
-            foreach (var entityKeyPair in ComponentDatas)
+            if (inputSource != null)
             {
-                var component = entityKeyPair.Key;
-                //var associatedData = entityKeyPair.Value;
+                foreach (var entityKeyPair in ComponentDatas)
+                {
+                    var component = entityKeyPair.Key;
+                    //var associatedData = entityKeyPair.Value;
 
-                if (component.Enabled)
-                    component.InputSource = inputSource;
-                else
-                    component.InputSource = null;
+                    if (component.Enabled)
+                        component.InputSource = inputSource;
+                    else
+                        component.InputSource = null;
+                }
             }
+
             base.Draw(context);
         }
 

VL.Stride.Runtime/src/Rendering/Renderers/WithWindowInputSource.cs
@@ -11,7 +11,7 @@ namespace VL.Stride.Rendering
 
         protected override void DrawInternal(RenderDrawContext context)
         {
-            var inputSource = InputSource;
+            var inputSource = InputSource ?? EmptyInputSource.Instance;
             using (context.RenderContext.PushTagAndRestore(InputExtensions.WindowInputSource, inputSource))
             {
                 DrawInput(context);