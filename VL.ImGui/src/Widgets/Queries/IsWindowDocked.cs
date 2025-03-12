7829e7c2860bdb253a136b041575470f349c3e96
antongit <antonworx@gmail.com>
2025-03-12 13:55:38 +0100

    [ImGui] Added IsWindowDocked query. Closes #7108.

---
+++ VL.ImGui/src/Widgets/Queries/IsWindowDocked.cs
@@ -0,0 +1,21 @@
+﻿namespace VL.ImGui.Widgets
+{
+    /// <summary>
+    /// Is current window docked into another window?
+    /// </summary>
+    [GenerateNode(Category = "ImGui.Queries", IsStylable = false)]
+    internal partial class IsWindowDocked : Query
+    {
+
+        public bool Value { get; private set; }
+
+        internal override void UpdateCore(Context context)
+        {
+            var capturedState = context.CapturedItemState;
+            if (capturedState.HasValue)
+                Value = capturedState.Value.IsEdited;
+            else
+                Value = ImGuiNET.ImGui.IsWindowDocked();
+        }
+    }
+}
