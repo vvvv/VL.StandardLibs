0625aea0a6c71ab5c6485ada7dc207188b5cb13e
Sebastian Gregor <gregor@vvvv.org>
2025-04-09 21:14:29 +0200

    default attribute works (tested for colors)

--- VL.Core/src/EditorAttributes/DefaultAttribute.cs
+++ VL.Core/src/EditorAttributes/DefaultAttribute.cs
@@ -9,38 +9,40 @@ namespace VL.Core.EditorAttributes
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
     public class DefaultAttribute : Attribute
     {
-        NodeContext NodeContext;
+        /// <param name="typeHint">Feeding a typeHint may abbreviate the serialization. Only feed a typeHint if you know that the type is known on deserialization.</param>
+        public static string ComputeEncodedValue(object value, NodeContext nodeContext = default, Type typeHint = default)
+        {
+            nodeContext = nodeContext ?? NodeContext.Create(AppHost.CurrentOrGlobal);
+            var optional = AttributeHelpers.EncodeValueForAttribute(value);
+            if (optional.HasValue)
+                return optional.Value;
+            else
+                return Serialization.Serialize(nodeContext, value, typeHint ?? typeof(object)).ToString();
+        }
 
         /// <summary>
         /// The default value of the property. Does not guearantee that the property gets initialized like that. Only a hint for editors.
         /// </summary>
-        /// <param name="value"></param>
-        /// <param name="nodeContext"></param>
-        /// <param name="typeHint">Feeding a typeHint may abbreviate the serialization. Only feed a typeHint if you know that the type is known on deserialization.</param>
-        public DefaultAttribute(object value, NodeContext nodeContext, Type typeHint = default)
+        public DefaultAttribute(string encodedValue)
         {
-            NodeContext = nodeContext ?? NodeContext.Create(AppHost.CurrentOrGlobal);
-            var optional = AttributeHelpers.EncodeValueForAttribute(value);
-            if (optional.HasValue)
-                EncodedValue = optional.Value;
-            else
-                EncodedValue = Serialization.Serialize(NodeContext, value, typeHint ?? typeof(object)).ToString();
+            EncodedValue = encodedValue;
         }
 
         public string EncodedValue { get; }
 
-        public T GetValue<T>() => (T)GetValue(typeof(T));
+        public T GetValue<T>(NodeContext nodeContext = default) => (T)GetValue(typeof(T), nodeContext);
 
-        public object GetValue(Type type)
+        public object GetValue(Type type, NodeContext nodeContext = default)
         {
+            nodeContext = nodeContext ?? NodeContext.Create(AppHost.CurrentOrGlobal);
             if (EncodedValue.StartsWith('<'))
-                return Serialization.Deserialize(NodeContext, XElement.Parse(EncodedValue), type);
+                return Serialization.Deserialize(nodeContext, XElement.Parse(EncodedValue), type);
 
             var optional = AttributeHelpers.DecodeValueFromAttribute(EncodedValue, type);
             if (optional.HasValue)
                 return optional.Value;
             else
-                return Serialization.Deserialize(NodeContext, XElement.Parse(EncodedValue), type);
+                return Serialization.Deserialize(nodeContext, XElement.Parse(EncodedValue), type);
         }
 
         public override string ToString()
