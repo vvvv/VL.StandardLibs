09d0100da22bf1aef37c2777e2a2a9766119ff42
Elias Holzer <elias@vvvv.org>
2025-05-06 17:03:31 +0200

    [ImGui] Adds widget to modify a TimeSpan

---
+++ VL.ImGui/src/Widgets/Input/InputTimeSpan.cs
@@ -0,0 +1,32 @@
+﻿using ImGuiNET;
+using VL.Core.EditorAttributes;
+
+namespace VL.ImGui.Widgets
+{
+    [GenerateNode(Name = "Input (TimeSpan)", Category = "ImGui.Widgets.Advanced")]
+    [WidgetType(WidgetType.Input)]
+    internal partial class InputTimeSpan : ChannelWidget<TimeSpan>, IHasInputTextFlags
+    {
+        public double Step { private get; set; } = 0.1d;
+
+        public double StepFast { private get; set; } = 10d;
+
+        /// <summary>
+        /// Adjust format string to decorate the value with a prefix, a suffix, or adapt the editing and display precision e.g. "%.3f" -> 1.234; "%5.2f secs" -> 01.23 secs; "Biscuit: % .0f" -> Biscuit: 1; etc.
+        /// </summary>
+        public string? Format { private get; set; } = "%.3f";
+
+        public ImGuiInputTextFlags Flags { get; set; }
+
+        TimeSpan lastframeValue;
+
+        internal override void UpdateCore(Context context)
+        {
+            var value = Update();
+            var seconds = value.TotalSeconds;
+            if (ImGuiUtils.InputDouble(widgetLabel.Update(label.Value), ref seconds, Step, StepFast, string.IsNullOrWhiteSpace(Format) ? null : Format, Flags))
+                SetValueIfChanged(lastframeValue, value = TimeSpan.FromSeconds(seconds), Flags);
+            lastframeValue = value;
+        }
+    }
+}
