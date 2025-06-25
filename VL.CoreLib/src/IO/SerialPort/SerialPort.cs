vvvv/VL.StandardLibs

commit 23f62bf5f5d40ce7021dfaac1f2d83ef4b1095e1

More fine grained control over direct import feature
- Thanks to new direct import features we can remove internal `VL.Stride.Runtime.Nodes` project
- Adds easy way to generate process nodes for a given C# class without having to write a full blown node factory
- Use import attribute for various C# written nodes. Replaces type forward -> operation forward -> hidden combos.

VL.CoreLib/src/IO/SerialPort/SerialPort.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.IO.Ports;
 using System.Linq;
 using System.Reactive.Linq;
+using VL.Core.Import;
 using VL.Lib.Basics.Resources;
 using VL.Lib.Collections;
+using SerialPort = VL.Lib.IO.Ports.SerialPort;
 using NetSerialPort = System.IO.Ports.SerialPort;
+
+[assembly: ImportType(typeof(SerialPort), Category = "IO.Ports")]
 
 namespace VL.Lib.IO.Ports
 {
@@ -56,6 +60,7 @@ namespace VL.Lib.IO.Ports
     /// <summary>
     /// Manages a serialport provider.
     /// </summary>
+    [ProcessNode]
     public class SerialPort
     {
         ResourceProviderMonitor<NetSerialPort> FCurrentProvider;
@@ -68,11 +73,6 @@ namespace VL.Lib.IO.Ports
         bool FDtrEnable;
         bool FRtsEnable;
         bool FBreakState;
-
-        /// <summary>
-        /// Whether or not the serialport is connected.
-        /// </summary>
-        public bool IsOpen => FCurrentProvider?.ResourcesUsedBySinks.FirstOrDefault()?.IsOpen ?? false;
 
         /// <summary>
         /// Configures the internally managed serialport provider.
@@ -124,5 +124,10 @@ namespace VL.Lib.IO.Ports
                 return FCurrentProvider;
             return null;
         }
+
+        /// <summary>
+        /// Whether or not the serialport is connected.
+        /// </summary>
+        public bool IsOpen => FCurrentProvider?.ResourcesUsedBySinks.FirstOrDefault()?.IsOpen ?? false;
     }
 }

commit b76b7a9c05a551d04f9498025ac2d8527a7b6d2a

Fixes default values on SerialPort node
Regression was introduced by 23f62bf5f5d40ce7021dfaac1f2d83ef4b1095e1

VL.CoreLib/src/IO/SerialPort/SerialPort.cs
@@ -8,6 +8,7 @@ using VL.Lib.Basics.Resources;
 using VL.Lib.Collections;
 using SerialPort = VL.Lib.IO.Ports.SerialPort;
 using NetSerialPort = System.IO.Ports.SerialPort;
+using System.ComponentModel;
 
 [assembly: ImportType(typeof(SerialPort), Category = "IO.Ports")]
 
@@ -78,7 +79,7 @@ namespace VL.Lib.IO.Ports
         /// Configures the internally managed serialport provider.
         /// </summary>
         /// <returns>A serialport provider which can be used by multiple threads in parallel.</returns>
-        public IResourceProvider<NetSerialPort> Update(ComPort portName, int baudrate, int dataBits, StopBits stopBits, Parity parity, Handshake handshake, bool dtrEnable, bool rtsEnable, bool breakState, bool open)
+        public IResourceProvider<NetSerialPort> Update(ComPort portName, [DefaultValue(115200)] int baudrate, [DefaultValue(8)] int dataBits, [DefaultValue(StopBits.One)] StopBits stopBits, Parity parity, Handshake handshake, bool dtrEnable, bool rtsEnable, bool breakState, bool open)
         {
             if (portName.Value != FPortName || baudrate != FBaudRate || dataBits != FDataBits || stopBits != FStopBits || parity != FParity || handshake != FHandshake || dtrEnable != FDtrEnable || rtsEnable != FRtsEnable || breakState != FBreakState)
             {