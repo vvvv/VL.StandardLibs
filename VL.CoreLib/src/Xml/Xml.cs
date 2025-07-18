0bdf3567dcb9ca564ac55927a939b083a5e1db9e
sebescudie <ping@sebescudie.fr>
2025-07-18 13:43:12 +0200

    Adds summaries, introduces GetJsonNamingPolicyFromEnum allowing to get an actual JsonNamingPolicy from a JsonPropertyNamingPolicy enum value

--- VL.CoreLib/src/Xml/Xml.cs
+++ VL.CoreLib/src/Xml/Xml.cs
@@ -14,6 +14,7 @@ using System.Xml.Xsl;
 using VL.Core;
 using VL.Lib.Basics.Resources;
 using VL.Lib.Collections;
+using System.Text.Json;
 
 namespace VL.Lib.Xml
 {
@@ -24,6 +25,9 @@ namespace VL.Lib.Xml
         Schema
     }
 
+    /// <summary>
+    /// User-facing enum allowing to pick a naming policy for objects serialized with the ObjectToJson node
+    /// </summary>
     public enum JsonPropertyNamingPolicy
     {
         CamelCase,
@@ -339,6 +343,25 @@ namespace VL.Lib.Xml
                 xslt.Transform(input.CreateReader(ReaderOptions.None), null, writer);
                 return writer.ToString();
             }
+        }
+
+        /// <summary>
+        /// Returns a JsonNamingPolicy from a JsonPropertyNamingPolicy
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static JsonNamingPolicy GetJsonNamingPolicyFromEnum(JsonPropertyNamingPolicy input)
+        {
+            return input switch
+            {
+                JsonPropertyNamingPolicy.CamelCase => JsonNamingPolicy.CamelCase,
+                JsonPropertyNamingPolicy.KebabCaseLower => JsonNamingPolicy.KebabCaseLower,
+                JsonPropertyNamingPolicy.KebabCaseUpper => JsonNamingPolicy.KebabCaseUpper,
+                JsonPropertyNamingPolicy.SnakeCaseLower => JsonNamingPolicy.SnakeCaseLower,
+                JsonPropertyNamingPolicy.SnakeCaseUpper => JsonNamingPolicy.SnakeCaseUpper,
+                _ => throw new ArgumentOutOfRangeException("input", input, "Could not create JsonNamingPolicy with provided value")
+            };
         }
     }
 }
