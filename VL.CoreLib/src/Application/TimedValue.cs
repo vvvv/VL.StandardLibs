b93b07c1d42850f127dc206bdd5adbd67873934f
Sebastian Gregor <gregor@vvvv.org>
2025-08-27 22:03:20 +0200

    pcb: Love for Monitor

---
+++ VL.CoreLib/src/Application/TimedValue.cs
@@ -0,0 +1,23 @@
+﻿namespace VL.Lib.Application
+{
+    public class TimedValue<T>
+    {
+        public TimedValue(double from, double to, T value)
+        {
+            From = from;
+            To = to;
+            Value = value;
+        }
+
+        public double From;
+        public double To;
+        public T Value;
+
+        public void Split(out double from, out double to, out T value)
+        {
+            from = From;
+            to = To;
+            value = Value;
+        }
+    }
+}
