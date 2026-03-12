48d54c15e585161160baae7cefcc33706b9d4f4a
Elias Holzer <elias@vvvv.org>
2026-03-12 19:18:34 +0100

    Adds SerializeTo and VL.Buffers package

--- VL.Serialization.MessagePack/src/MessagePackSerialization.cs
+++ VL.Serialization.MessagePack/src/MessagePackSerialization.cs
@@ -1,15 +1,34 @@
 ﻿using MessagePack;
 using System;
+using System.Buffers;
 using System.Collections.Generic;
+using System.IO;
+using VL.Core;
+using VL.Core.Import;
 using VL.Serialization.MessagePack.Resolvers;
 
 namespace VL.Serialization.MessagePack
 {
+    [SkipCategory]
     public static class MessagePackSerialization
     {
         public static byte[] Serialize<T>(T input)
         {
             return MessagePackSerializer.Serialize(input, VLResolver.Options);
+        }
+
+        public static TWriter SerializeTo<TWriter, T>(TWriter writer, T input)
+            where TWriter : IBufferWriter<byte>
+        {
+            MessagePackSerializer.Serialize(writer, input, VLResolver.Options);
+            return writer;
+        }
+
+        public static TStream SerializeToStream<TStream, T>(TStream stream, T input)
+            where TStream : Stream
+        {
+            MessagePackSerializer.Serialize(stream, input, VLResolver.Options);
+            return stream;
         }
 
         public static T Deserialize<T>(IEnumerable<byte> input)
@@ -20,6 +39,7 @@ namespace VL.Serialization.MessagePack
             throw new ArgumentException($"Couldn't retrieve memory from {typeof(T)}", nameof(input));
         }
 
+        [Smell(SymbolSmell.Hidden)]
         public static T Deserialize<T>(ReadOnlyMemory<byte> input)
         {
             return MessagePackSerializer.Deserialize<T>(input, VLResolver.Options);
@@ -31,6 +51,12 @@ namespace VL.Serialization.MessagePack
             if (prettify)
                 return JsonHelper.FormatJson(json);
             return json;
+        }
+
+        public static TextWriter SerializeJsonTo<T>(T input, TextWriter writer)
+        {
+            MessagePackSerializer.SerializeToJson(writer, input, VLResolver.Options);
+            return writer;
         }
 
         public static T DeserializeJson<T>(string input)
